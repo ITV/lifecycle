34a7233c86fc71f0c217f7d652b47793f212ea67
Martin Carolan <martin.carolan@itv.com>
2017-05-24 19:37:34 +0100

    Also release for version release tags

--- ci/publish.sh
+++ ci/publish.sh
@@ -9,10 +9,11 @@ eval "$sbt_cmd clean compile test"
 
 echo "TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST"
 echo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
+echo "TRAVIS_TAG = $TRAVIS_TAG"
 version=`cat version.sbt`
 echo "version = $version"
 
-if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
+if [[ $TRAVIS_TAG =~ ^v[0-9].* || ($TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master") ]]; then
   openssl aes-256-cbc -K $encrypted_580469430836_key -iv $encrypted_580469430836_iv -in ci/private.asc.enc -out ci/private.asc -d
   eval "$sbt_cmd clean +publishSigned"
 fi;
