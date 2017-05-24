66b45d7bddef18a3aff1c234662defe01a8fa765
Martin Carolan <martin.carolan@itv.com>
2017-05-24 19:14:04 +0100

    Publish script debugging, and try to push release commit seperately to tag to not confuse travis

--- ci/publish.sh
+++ ci/publish.sh
@@ -7,6 +7,11 @@ sbt_cmd="sbt ++$TRAVIS_SCALA_VERSION"
 
 eval "$sbt_cmd clean compile test"
 
+echo "TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST"
+echo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
+version=`cat version.sbt`
+echo "version = $version"
+
 if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
   openssl aes-256-cbc -K $encrypted_580469430836_key -iv $encrypted_580469430836_iv -in ci/private.asc.enc -out ci/private.asc -d
   eval "$sbt_cmd clean +publishSigned"
